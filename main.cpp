ffd726a3f25d16bcb4a525ecca96123539889c66
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-10-16 15:04:39 -0500

    Added function to read file into string

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <fstream>
 
-void patternSearch(){
+std::string readFileIntoString(const std::string path) {
+    std::ifstream input_file(path);
+    if (!input_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
 
+void patternSearch(){
+    
 }
 
 void longestPalindromicSubstring(){
@@ -22,7 +32,7 @@ int main(int argc, char const *argv[]){
 
 
     longestCommonSubstring();
-    
+
 
     return 0;
 }
