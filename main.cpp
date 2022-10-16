803fec099ad254cc82e347b3b0e723351c71be57
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-10-16 14:46:53 -0500

    Inital Commit - Basic Structure

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+void patternSearch(){
+
+}
+
+void longestPalindromicSubstring(){
+
+}
+
+void longestCommonSubstring(){
+
+}
+
+int main(int argc, char const *argv[]){
+
+    patternSearch();
+
+
+    longestPalindromicSubstring();
+
+
+    longestCommonSubstring();
+    
+
+    return 0;
+}
